4c3ede5c40482917571a9e6b3d96d3886f95fb82
ytksh <119751708+ytksh@users.noreply.github.com>
2024-02-05 23:03:46 -0300

    stalker themed website

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,42 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../services/products.service';
 import { Product, Products } from '../types';
+import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) { }
 
-  ngOnInit() {
+  totalRecords: number = 0;
+  rows: number = 5;
+
+  products: Product[] =  []; 
+  onProductOutput(product: Product) {
+    console.log(product, "Product");
+  }
+
+  fetchProducts(page: number, perPage: number){
     this.productsService
-    .getProducts('http://localhost:3000/clothes', {page: 0, perPage: 5})
+    .getProducts('http://localhost:3000/clothes', {page: page, perPage: perPage})
     .subscribe((products: Products) => {
-      console.log(products.items);
+      this.products = products.items;
+      this.totalRecords = products.total;
     });
+  }
+
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
+  }
+  ngOnInit() {
+    this.fetchProducts(0, this.rows);
 
   }
 }
